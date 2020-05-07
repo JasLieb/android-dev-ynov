520df42082e422620ded03ff9f521b5fd6ce259b
Jason Liebault <jason.liebault@simsoft-industry.fr>
2020-05-07 14:59:15 +0200

    Fix typo

--- ScheduleApp/app/src/main/java/com/jaslieb/scheduleapp/models/family/Family.java
+++ ScheduleApp/app/src/main/java/com/jaslieb/scheduleapp/models/family/Family.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class Family {
     String name;
     ArrayList<Parent> parents;
-    ArrayList<Child> childs;
+    ArrayList<Child> children;
 
     public Family() {}
 
-    public Family(String name, ArrayList<Parent> parents, ArrayList<Child> childs) {
+    public Family(String name, ArrayList<Parent> parents, ArrayList<Child> children) {
         this.name = name;
         this.parents = parents;
-        this.childs = childs;
+        this.children = children;
     }
 }
